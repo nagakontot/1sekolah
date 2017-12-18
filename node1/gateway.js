0c6a2c48cddfaafb61609f6a6e22de36d0b09d7d
nagakontot <nagakontot@gmail.com>
2017-12-18 05:20:26 +0000

    add res dir and  files

--- node1/gateway.js
+++ node1/gateway.js
@@ -35,44 +35,44 @@ redbird.register("www.1sekolah.xyz",                "http://172.18.0.3:6000",   
 redbird.register("ide.1sekolah.xyz",                "http://172.18.0.3:8181",   {ssl: true});
 
 //redbird.register("test1.1sekolah.xyz",            "http://172.18.0.2:3500",   {ssl: true});
-//redbird.register("test1.1sekolah.xyz",            "http://172.18.0.2:6000",   {ssl: true});
-
+redbird.register("test1.1sekolah.xyz",              "http://172.18.0.3:8080",   {ssl: true});
 
 //redbird.register("socket.1sekolah.xyz",           "http://172.18.0.2:3500",   {ssl: true});    //using ws
 //redbird.register("socket.1sekolah.xyz",           "http://172.18.0.2:3600",   {ssl: true});      //using uws
 redbird.register("socket.1sekolah.xyz",             "http://172.18.0.3:6000",   {ssl: true});      //using uws
 
+redbird.register("droppy.1sekolah.xyz",             "http://172.18.0.2:8989",   {ssl: true});
+
 //redbird.register("cdn.1sekolah.xyz",               "http://172.18.0.4:9000",   {ssl: true});      
 
 //redbird.register("cors.1sekolah.xyz",               "http://172.18.0.4:8080",   {ssl: true});    
-//redbird.register("droppy.1sekolah.xyz",           "http://172.18.0.3:8989",   {ssl: true});
 
-//redbird.register("x11.1sekolah.xyz",            "http://172.18.0.3:10000",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=Keplaotak_1234
+//redbird.register("x11.1sekolah.xyz",              "http://172.18.0.3:10000",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=Keplaotak_1234
 
 //redbird.register("wine.1sekolah.xyz",            "http://172.18.0.4:8080",   {ssl: true});
-//redbird.register("vpn.1sekolah.xyz",            "http://172.18.0.4:5555",   {ssl: true});
+//redbird.register("vpn.1sekolah.xyz",              "http://172.18.0.4:5555",   {ssl: true});
 
 
-//redbird.register("test1.1sekolah.xyz",          "http://172.18.0.2:8080",   {ssl: true});
-//redbird.register("socket.1sekolah.xyz",         "http://172.18.0.5:6020",   {ssl: true});
+//redbird.register("test1.1sekolah.xyz",            "http://172.18.0.2:8080",   {ssl: true});
+//redbird.register("socket.1sekolah.xyz",           "http://172.18.0.5:6020",   {ssl: true});
 
-//redbird.register("rchat.1sekolah.xyz",          "http://172.18.0.4:3000",   {ssl: true});
+redbird.register("rchat.1sekolah.xyz",              "http://172.18.0.5:3000",   {ssl: true});
 
-//redbird.register("test2.1sekolah.xyz",          "http://172.18.0.5:80",  {ssl: true});
+//redbird.register("test2.1sekolah.xyz",            "http://172.18.0.5:80",  {ssl: true});
 
-//redbird.register("ide.1sekolah.xyz",            "http://172.18.0.4:8181",   {ssl: true});
+//redbird.register("ide.1sekolah.xyz",              "http://172.18.0.4:8181",   {ssl: true});
 
 
 //redbird.register("drupal.1sekolah.xyz",           "http://172.18.0.6:80",  {ssl: true});
 
-//redbird.register("x11.1sekolah.xyz",            "http://172.18.0.4:20000",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=keplaotak
-//redbird.register("wine.1sekolah.xyz",           "http://172.18.0.8:8080",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=keplaotak
+//redbird.register("x11.1sekolah.xyz",              "http://172.18.0.4:20000",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=keplaotak
+//redbird.register("wine.1sekolah.xyz",             "http://172.18.0.8:8080",  {ssl: true});   ////Then visit https://x11.1sekolah.xyz/index.html?encoding=png&password=keplaotak
                                          
 //redbird.register("canvas.1sekolah.xyz",           "http://172.18.0.6:3000",  {ssl: true});
 //redbird.register("moodle.1sekolah.xyz",           "http://172.18.0.4:80",  {ssl: true});
 //redbird.register("moodle.1sekolah.xyz",           "https://172.18.0.4:443",  {ssl: true});
 
-//redbird.register("wp.1sekolah.xyz",           "https://172.18.0.5:80",  {ssl: true});
+//redbird.register("wp.1sekolah.xyz",               "https://172.18.0.5:80",  {ssl: true});
 
 
 
