b06224f15acb7ddc1b3d6fef2efea61f6577e460
nagakontot <nagakontot@gmail.com>
2017-12-28 17:24:28 +0000

    add dosbox

--- node1/gateway.js
+++ node1/gateway.js
@@ -43,6 +43,7 @@ redbird.register("socket.1sekolah.xyz",             "http://172.18.0.3:6000",   
 
 redbird.register("droppy.1sekolah.xyz",             "http://172.18.0.2:8989",   {ssl: true});
 
+//redbird.register("browser.1sekolah.xyz",             "http://172.18.0.4:6080",   {ssl: true});      
 //redbird.register("cdn.1sekolah.xyz",               "http://172.18.0.4:9000",   {ssl: true});      
 
 //redbird.register("cors.1sekolah.xyz",               "http://172.18.0.4:8080",   {ssl: true});    
@@ -56,7 +57,7 @@ redbird.register("droppy.1sekolah.xyz",             "http://172.18.0.2:8989",   
 //redbird.register("test1.1sekolah.xyz",            "http://172.18.0.2:8080",   {ssl: true});
 //redbird.register("socket.1sekolah.xyz",           "http://172.18.0.5:6020",   {ssl: true});
 
-redbird.register("rchat.1sekolah.xyz",              "http://172.18.0.5:3000",   {ssl: true});
+//redbird.register("rchat.1sekolah.xyz",              "http://172.18.0.5:3000",   {ssl: true});
 
 //redbird.register("test2.1sekolah.xyz",            "http://172.18.0.5:80",  {ssl: true});
 
